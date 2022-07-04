7df893be2e30d58335572b02f7c3913ebb0d9d1a
Thinkersbluff <36551518+Thinkersbluff@users.noreply.github.com>
2022-07-03 21:03:47 -0400

    Moved bilinear levelling code into a class similar to mbl and ubl  (#4)
    * Fixed uninitialised variable bug per MarlinFirmware#23791
    
    Although the G29 routine displays all zeroes as part of the process, the Marlin Pull Request says those values remained cached and used in error by the firmware when printing.
    This is the entire change on record upstream to correct the bug.
    
    * Renamed bed leveling only file to bbl (i.e. not general auto bed leveling
    
    * Moved bilinear levelling code into a class similar to mbl and ubl
    
    Implemented the changes in
    commit 704beafb7effab7d8f2890378f0383ef8cf25d12
    
    NOTE: One apparent contradiction between this commit and the previous edits to G29.cpp :(

---
+++ Marlin/src/feature/bedlevel/abl/bbl.h
@@ -0,0 +1,65 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+#include "../../../inc/MarlinConfigPre.h"
+
+class bilinear_bed_leveling {
+  static void extrapolate_one_point(const uint8_t x, const uint8_t y, const int8_t xdir, const int8_t ydir);
+
+  #if ENABLED(ABL_BILINEAR_SUBDIVISION)
+    #define ABL_GRID_POINTS_VIRT_X (GRID_MAX_CELLS_X * (BILINEAR_SUBDIVISIONS) + 1)
+    #define ABL_GRID_POINTS_VIRT_Y (GRID_MAX_CELLS_Y * (BILINEAR_SUBDIVISIONS) + 1)
+    static float z_values_virt[ABL_GRID_POINTS_VIRT_X][ABL_GRID_POINTS_VIRT_Y];
+    static xy_pos_t grid_spacing_virt;
+    static xy_float_t grid_factor_virt;
+
+    static float bed_level_virt_coord(const uint8_t x, const uint8_t y);
+    static float bed_level_virt_cmr(const float p[4], const uint8_t i, const float t);
+    static float bed_level_virt_2cmr(const uint8_t x, const uint8_t y, const_float_t tx, const_float_t ty);
+#endif
+
+public:
+  static xy_pos_t grid_spacing, grid_start;
+  static xy_float_t grid_factor;
+  static bed_mesh_t z_values;
+
+  static float z_offset(const xy_pos_t &raw);
+
+  static void extrapolate_unprobed_bed_level();
+  static void print_leveling_grid();
+  static void refresh_bed_level();
+  #if ENABLED(ABL_BILINEAR_SUBDIVISION)
+    static void print_leveling_grid_virt();
+    static void bed_level_virt_interpolate();
+  #endif
+
+  #if IS_CARTESIAN && DISABLED(SEGMENT_LEVELED_MOVES)
+    static void line_to_destination(const_feedRate_t scaled_fr_mm_s, uint16_t x_splits=0xFFFF, uint16_t y_splits=0xFFFF);
+  #endif
+};
+
+extern bilinear_bed_leveling bbl;
+
+#define _GET_MESH_X(I) float(bbl.grid_start.x + (I) * bbl.grid_spacing.x)
+#define _GET_MESH_Y(J) float(bbl.grid_start.y + (J) * bbl.grid_spacing.y)
+#define Z_VALUES_ARR bbl.z_values
