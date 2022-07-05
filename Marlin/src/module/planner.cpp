890bfbb00f002a88bd017699f8ff82c943cae0eb
Thinkersbluff <36551518+Thinkersbluff@users.noreply.github.com>
2022-07-05 00:45:47 -0400

    White space changes

--- Marlin/src/module/planner.cpp
+++ Marlin/src/module/planner.cpp
@@ -1563,42 +1563,35 @@ void Planner::check_axes_activity() {
   }
 
   void Planner::unapply_leveling(xyz_pos_t &raw) {
+    if (!leveling_active) return;
+    #if ABL_PLANAR
 
-    if (leveling_active) {
+    matrix_3x3 inverse = matrix_3x3::transpose(bed_level_matrix);
 
-      #if ABL_PLANAR
+    xy_pos_t d = raw - level_fulcrum;
+    inverse.apply_rotation_xyz(d.x, d.y, raw.z);
+    raw = d + level_fulcrum;
 
-        matrix_3x3 inverse = matrix_3x3::transpose(bed_level_matrix);
+    #elif HAS_MESH
 
-        xy_pos_t d = raw - level_fulcrum;
-        inverse.apply_rotation_xyz(d.x, d.y, raw.z);
-        raw = d + level_fulcrum;
+      #if ENABLED(ENABLE_LEVELING_FADE_HEIGHT)
+        const float fade_scaling_factor = fade_scaling_factor_for_z(raw.z);
+      #elif DISABLED(MESH_BED_LEVELING)
+        constexpr float fade_scaling_factor = 1.0;
+      #endif
 
-      #elif HAS_MESH
-
-        #if ENABLED(ENABLE_LEVELING_FADE_HEIGHT)
-          const float fade_scaling_factor = fade_scaling_factor_for_z(raw.z);
-        #elif DISABLED(MESH_BED_LEVELING)
-          constexpr float fade_scaling_factor = 1.0;
+       raw.z -= (
+        #if ENABLED(MESH_BED_LEVELING)
+          mbl.get_z(raw OPTARG(ENABLE_LEVELING_FADE_HEIGHT, fade_scaling_factor))
+        #elif ENABLED(AUTO_BED_LEVELING_UBL)
+          fade_scaling_factor ? fade_scaling_factor * ubl.get_z_correction(raw) : 0.0
+        #elif ENABLED(AUTO_BED_LEVELING_BILINEAR)
+          fade_scaling_factor ? fade_scaling_factor * bbl.get_z_correction(raw) : 0.0
         #endif
-
-        raw.z -= (
-          #if ENABLED(MESH_BED_LEVELING)
-            mbl.get_z(raw
-              #if ENABLED(ENABLE_LEVELING_FADE_HEIGHT)
-                , fade_scaling_factor
-              #endif
-            )
-          #elif ENABLED(AUTO_BED_LEVELING_UBL)
-            fade_scaling_factor ? fade_scaling_factor * ubl.get_z_correction(raw) : 0.0
-          #elif ENABLED(AUTO_BED_LEVELING_BILINEAR)
-            fade_scaling_factor ? fade_scaling_factor * bbl.get_z_correction(raw) : 0.0
-          #endif
         );
 
       #endif
     }
-  }
 
 #endif // HAS_LEVELING
 
